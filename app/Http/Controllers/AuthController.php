811990056f7f9bcef848b6fd320efd3256ce43ae
Keval Gangani <keval.gangani@gmail.com>
2023-07-24 12:56:43 +0530

    Added register api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'first_name' => $request->input(key:'first_name'),
+            'last_name' => $request->input(key:'last_name'),
+            'email' => $request->input(key:'email'),
+            'password' => Hash::make($request->input(key:'password')),
+        ]);
+
+        return response($user, status: Response::HTTP_CREATED);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get(uri: 'hello', fn() => 'hello world');
+Route::post('register', [AuthController::class, 'register']);
